3bd4f0652d521361ced657565f65389bb33739ef
Luis Yucra <luis.yucra@gmail.com>
2020-11-28 01:45:52 +0000

    fix test

--- tests/UserManagerTest.php
+++ tests/UserManagerTest.php
@@ -8,15 +8,17 @@ final class UserManagerTest extends TestCase
 {
     public function testRecordPurchase()
     {
+	global $CFG;    
+	    
 	$userManager = new UserManager();
 	$userManager->recordPurchase(10, 11, 12);
 
 	$dbConnection = mysqli_connect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass, $CFG->dbname);
-	$result = $mysqli_query($dbConnection, 'SELECT * FROM purchase');
+	$result = mysqli_query($dbConnection, 'SELECT * FROM purchase');
 	$row = $result->fetch_assoc();
-	$this->assertEquals(10, $row['item_code']);
-	$this->assertEquals(11, $row['price']);
-	$this->assertEquals(12, $row['user_id']);
+        $this->assertEquals(10, $row['user_id']);
+	$this->assertEquals(11, $row['item_code']);
+	$this->assertEquals(12, $row['price']);
     }
 
 }
